1ce5287b9990cd60023bab88f44f4f301561d9e7
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2022-01-13 23:22:19 +0700

    ship panel

--- j.js
+++ j.js
@@ -1,6 +1,7 @@
 (function () {
   let matrix = [];
   const board = document.getElementById("board");
+  const shipPanel = document.getElementById("shipPanel");
 
   const CreateBoard = () => {
     for (var i = 0; i < 10; i++) {
@@ -19,14 +20,42 @@
 
   CreateBoard();
 
-  board.addEventListener('click', function(e) {
+  board.addEventListener("click", function (e) {
     const idCell = e.target.id;
-    indexMatrix = idCell.split(' ');
+    indexMatrix = idCell.split(" ");
     document.getElementById(idCell).innerHTML = "X";
-    
-    matrix[indexMatrix[0]] [indexMatrix[1]] = "X";
-    console.log(matrix[0][0])
-  })
 
-  
+    matrix[indexMatrix[0]][indexMatrix[1]] = "X";
+    console.log(matrix[0][0]);
+  });
+
+  const shipDeck = {
+    one: { cell: 1, count: 4 },
+    two: { cell: 2, count: 3 },
+    three: { cell: 3, count: 2 },
+    four: { cell: 4, count: 1 },
+  };
+
+  const createShip = (countCell) => {
+    const ship = document.createElement("div");
+    ship.setAttribute("class", "ship");
+    for (let i = 0; i < countCell; i++) {
+      const deck = document.createElement("div");
+      ship.appendChild(deck);
+    }
+    return ship;
+  };
+
+  const initateShips = () => {
+    for (var key in shipDeck){
+        console.log(shipDeck[key].count)
+        for (let i = 0; i < shipDeck[key].count; i++) {
+            let ship = createShip(shipDeck[key].cell)
+            shipPanel.appendChild(ship);
+        } 
+    }
+
+  }
+
+ initateShips()
 })();
